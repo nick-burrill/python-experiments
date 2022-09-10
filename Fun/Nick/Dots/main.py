4477d8eb5a1a97d40ad05925c1d07e85301662b2
Nick <nickpburrill@gmail.com>
2022-09-10 16:35:51 -0400

    Experimentation on dots project ⌛

--- Fun/Nick/Dots/main.py
+++ Fun/Nick/Dots/main.py
@@ -4,7 +4,7 @@ colorTable = {"water": (0, 0, 255), "sand": (255, 0, 0), "air": (0, 255, 0)}
 
 
 def pixel(surface, color, pos):
-    pygame.draw.circle(surface, color, pos, 0)
+    pygame.draw.circle(surface, color, pos, 1)
 
 
 class Dot:
@@ -21,17 +21,27 @@ class DotMap:
         for row in range(self.height):
             for col in range(self.width):
                 # print((row, col))
-                self.array[row][col] = Dot("air")
+                self.array[row][col] = Dot("water")
 
     def add(self, pos, type):
         self.array[pos] = Dot(type)
 
 
+# def DrawDots(surface, map, table):
+#     for i, item in enumerate(map.array):
+#         for j in range(len(item)):
+#             color = table.get(j.type)
+#             pixel(surface, color, (i, j))
 def DrawDots(surface, map, table):
-    for i, item in enumerate(map.array):
-        for j in range(len(item)):
+    x = 0
+    y = 0
+    for i in map.array:
+        y = y + 1
+        x = 0
+        for j in i:
+            x = x + 1
             color = table.get(j.type)
-            pixel(surface, color, (i, j))
+            pixel(surface, color, (y, x))
 
 
 def main():
@@ -52,9 +62,11 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONUP:
+                print("mouseup")
                 # pos = pygame.mouse.get_pos()
                 # dotMap.add(pos, "sand")
                 DrawDots(screen, dotMap, colorTable)
+                pixel(screen, (255, 55, 55), (10, 10))
 
 
 if __name__ == "__main__":
